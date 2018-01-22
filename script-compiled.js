ce187f3bc502f72f093f8576f8f560b651160b65
Grzegorz Kluka <g.kluka@wp.pl>
2018-01-22 14:27:13 +0100

    First commit

---
+++ script-compiled.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//zadanie 1
+var a = "Hello";
+var b = "World";
+
+console.log(a + " " + b);
---
+++ script.js
@@ -0,0 +1,5 @@
+//zadanie 1
+const a = `Hello`;
+const b = `World`;
+
+console.log(`${a} ${b}`)
